d2037f23dcfcbb4fcfbac6395200c497afa74b25
Kamillo Ferry <kafe@kafe.dev>
2024-08-27 14:59:18 +0300

    Add test for precompilation

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,20 @@ using Oscar, GroebnerWalk
 using Test
 
 @testset verbose=true "All tests" begin
+  @testset "Compilation" begin
+    R,(x,y) = polynomial_ring(QQ, [:x,:y])
+    I = ideal([
+      x^3*y - 2x^2*y^2 + x,
+      3x^4 - y
+    ])
+
+    Gs = groebner_walk(I, lex(R)) |> gens |> Oscar.IdealGens
+    @test is_groebner_basis(Gs; ordering=lex(R))
+
+    Gg = groebner_walk(I, lex(R); algorithm=:generic) |> gens |> Oscar.IdealGens
+    @test is_groebner_basis(Gg; ordering=lex(R))
+  end
+
   include("standard-walk.jl")
   include("generic-walk.jl")
 end
--- test/standard-walk.jl
+++ test/standard-walk.jl
@@ -1,8 +1,11 @@
 @testset "Standard walk" begin
-    R1, (x,y,z) = polynomial_ring(QQ, ["x", "y", "z"])
+    R1, (x,y,z) = polynomial_ring(QQ, [:x,:y,:z])
             
     I1 = ideal([x^2 + y*z, x*y + z^2]) 
-    G1 = groebner_basis(I1)
+    # The Oscar.IdealGens type stores whether the generators form a GB.
+    # To test whether the generators really form a GB, we recreate the object.
+    G1 = groebner_walk(I1) |> gens |> Oscar.IdealGens
+    @test is_groebner_basis(G1; ordering=lex(R1))
 
     I2 = ideal([z^3 - z^2 - 4])
     G2 = groebner_basis(I2)
@@ -10,7 +13,6 @@
     I3 = ideal([x^6 - 1])
     G3 = groebner_basis(I3)
 
-    @test is_groebner_basis(groebner_walk(I1); ordering=lex(R1))
     #@test bounding_vectors(G1) == Vector{ZZRingElem}.([[1,1,-2],[2,-1,-1], [-1,2,-1]])
     #@test next_weight(G1, ZZ.([1,1,1]), ZZ.([1,0,0])) == ZZ.([1,1,1])
 

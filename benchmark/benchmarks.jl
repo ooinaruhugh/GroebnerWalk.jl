259adbae55e1f810318055e85770edcf38b73950
Kamillo Ferry <kafe@kafe.dev>
2024-12-17 15:42:02 +0100

    Rework the benchmarking code

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,11 +1,14 @@
 using Oscar
-using GroebnerWalk
+#using GroebnerWalk
 using BenchmarkTools
 
 include("cyclic.jl")
 include("agk.jl")
 include("tran3.3.jl")
 
+const MAX_TIME = 1000
+const N_SAMPLES = 3
+const N_EVALS = 3
 function benchmark(
   io,
   name::String,
@@ -13,19 +16,27 @@ function benchmark(
   target::MonomialOrdering,
   start::MonomialOrdering
 )
+  print(io, gethostname(), ",")
+  print("Benchmarking $(name)...")
   print(io, name, ","); flush(io)
-  t = @belapsed groebner_walk($I, $target, $start; algorithm=:standard) seconds=20000 samples=10
+  t = @belapsed groebner_walk($I, $target, $start; algorithm=:standard) seconds=MAX_TIME samples=N_SAMPLES evals=N_EVALS
   print(io, t, ","); flush(io)
-  t = @belapsed groebner_walk($I, $target, $start; algorithm=:generic) seconds=20000 samples=10
+  t = @belapsed groebner_walk($I, $target, $start; algorithm=:generic) seconds=MAX_TIME samples=N_SAMPLES evals=N_EVALS
+  #print(io, t, ","); flush(io)
+  #t = @belapsed groebner_walk($I, $target, $start; algorithm=:perturbed) seconds=20000 samples=10
   print(io, t, ","); flush(io)
-  t = @belapsed groebner_walk($I, $target, $start; algorithm=:perturbed) seconds=20000 samples=10
-  print(io, t, ","); flush(io)
-  t = @belapsed groebner_basis($I; ordering=$target) seconds=20000 samples=10
+  t = @belapsed begin 
+    J = ideal(gens($I))
+    groebner_basis(J; ordering=$target) 
+  end seconds=MAX_TIME samples=N_SAMPLES evals=N_EVALS
   println(io, t); flush(io)
+
+  println("\t\t\t[done]")
 end
 
 function print_header(io)
-  print(io, "name,standard_walk,generic_walk,perturbed_walk,buchberger\n")
+  #print(io, "name,standard_walk,generic_walk,perturbed_walk,buchberger\n")
+  print(io, "hostname,name,standard_walk,generic_walk,buchberger\n")
 end
 
 p = 11863279
@@ -51,8 +62,8 @@ open("results.csv", "a") do io
   Rp = base_ring(Ip)
   benchmark(io, "katsura6-Fp", Ip, lex(Rp), default_ordering(Rp))
 
-  benchmark(io, "cyclic7-QQ", cyclic7(QQ)...)
-  benchmark(io, "cyclic7-Fp", cyclic7(Fp)...)
+  #benchmark(io, "cyclic7-QQ", cyclic7(QQ)...)
+  #benchmark(io, "cyclic7-Fp", cyclic7(Fp)...)
 
   benchmark(io, "agk4-QQ", agk4(QQ)...)
   benchmark(io, "agk4-Fp", agk4(Fp)...)
